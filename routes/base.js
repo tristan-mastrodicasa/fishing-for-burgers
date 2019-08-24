8741e258ad281a5ef117da70fc382f65edbd4287
吉喆 <jizhe@chuangyejia.com>
2019-08-25 10:58:57 +1200

    change base path

--- routes/base.js
+++ routes/base.js
@@ -6,7 +6,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.render('pages/burger_detail');
+    res.render('pages/index');
 });
 
 module.exports = router;
