f83620f4239bb3e84212febded9b4cb6d7cea79d
Tristan <ghoststeam217@gmail.com>
2019-08-24 14:39:17 +1200

    MAJOR: Boilerplate code

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Entry file for web server
+ * @author Tristan Mastrodicasa
+ */
+
+const express = require('express');
+
+// Import the route handlers
+const baseRoute = require('./routes/base');
+
+const port = process.env.PORT || 80;
+const app = express();
+
+// Config
+app.set('view engine', 'ejs');
+
+// app.use(bodyParser.urlencoded({extended: false})); Might need????
+
+// Set route handlers
+app.use('/', baseRoute);
+
+// Allow serving static assets from public folder
+app.use('/public', express.static('public'));
+
+app.listen(port);
