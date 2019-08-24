a41d1e4c18798ebe7cd853d6ac4a2c501a3c91b2
Josh <josh.espinas@xero.com>
2019-08-24 22:27:03 +1200

    using npm start script and logs out what port is active

--- index.js
+++ index.js
@@ -3,23 +3,23 @@
  * @author Tristan Mastrodicasa
  */
 
-const express = require('express');
+const express = require("express");
 
 // Import the route handlers
-const baseRoute = require('./routes/base');
+const baseRoute = require("./routes/base");
 
 const port = process.env.PORT || 80;
 const app = express();
 
 // Config
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // app.use(bodyParser.urlencoded({extended: false})); Might need????
 
 // Set route handlers
-app.use('/', baseRoute);
+app.use("/", baseRoute);
 
 // Allow serving static assets from public folder
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
-app.listen(port);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
