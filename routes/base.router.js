02f7911f2a22992f882a96a74f1209fc3ca7dda7
Tristan <ghoststeam217@gmail.com>
2019-08-25 16:19:42 +1200

    Fixed styles

--- routes/base.router.js
+++ routes/base.router.js
@@ -22,7 +22,7 @@ router.get('/', (req, res, next) => {
  * @apiGroup Index
  */
 router.get('/price_input', (req, res, next) => {
-	res.render('pages/price-input');
+	res.render('pages/page-input');
 });
 
 /**
